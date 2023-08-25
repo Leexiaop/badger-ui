a35e445ee9252ad525b5ee3b0beaf4b30fc5c6e1
Leexiaop <282191344@qq.com>
2023-08-25 14:48:38 +0800

    提交

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@
  * @Author: Leexiaop 282191344@qq.com
  * @Date: 2023-07-17 11:46:39
  * @LastEditors: Leexiaop 282191344@qq.com
- * @LastEditTime: 2023-08-24 17:40:42
+ * @LastEditTime: 2023-08-25 14:48:08
  * @FilePath: \badger-ui\.umirc.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ const repo = 'badger-ui';
 export default defineConfig({
 	title: '前端平头哥',
 	favicon: './public/logo.ico',
-	logo: 'http://leexiaop.github.io/statics/ibadgers/logo.png',
+	logo: 'http://leexiaop.github.io/static/ibadgers/logo.png',
 	mode: 'site',
 	hash: true,
 	base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
