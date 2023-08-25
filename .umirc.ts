f3334cfa8f2f8932d5e1495cf907067f1986850e
Leexiaop <282191344@qq.com>
2023-08-25 14:58:08 +0800

    dddd

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@
  * @Author: Leexiaop 282191344@qq.com
  * @Date: 2023-07-17 11:46:39
  * @LastEditors: Leexiaop 282191344@qq.com
- * @LastEditTime: 2023-08-25 14:48:08
+ * @LastEditTime: 2023-08-25 14:49:55
  * @FilePath: \badger-ui\.umirc.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
